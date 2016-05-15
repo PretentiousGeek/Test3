7ccd6351c99581a22b358ff9c37f2819e38812c1
Pretentious Geek <cai.revell@gmail.com>
2016-05-15 14:32:23 +0100

    Added the GLExplore window code
    See 'com.nashlaga.core/Main.java'.

---
+++ src/com/nashlaga/core/Main.java
@@ -0,0 +1,71 @@
+package com.nashlaga.core;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*; // Gives OpenGL access.
+import static org.lwjgl.system.MemoryUtil.*; // Gives us NULL
+
+import java.nio.ByteBuffer;
+import org.lwjgl.glfw.GLFWVidMode;
+
+public class Main implements Runnable{
+	
+	private Thread thread;
+	public boolean running = true;
+	
+	public Long window;
+	
+	public static void main(String[] args) {
+		// Main method
+		Main game = new Main();
+		game.start();
+	}
+	
+	public void start(){
+		running = true;
+		thread = new Thread(this, "GLExplore");
+		thread.start();
+	}
+	
+	public void init(){
+		if(!glfwInit()){
+			System.err.println("GLFW init failure!");
+		}
+		
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		
+		window = glfwCreateWindow(800, 600, "GLExplore", NULL, NULL);
+		
+		if(window == NULL){
+			System.err.println("Failed to create window!");
+		}
+		
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(window, 100, 100);
+		
+		glfwMakeContextCurrent(window);
+		
+		glfwShowWindow(window);
+	}
+	
+	public void update(){
+		glfwPollEvents();
+	}
+	
+	public void render(){
+		glfwSwapBuffers(window);
+	}
+	
+	@Override
+	public void run(){
+		init();
+		while(running){
+			update();
+			render();
+			
+			if(glfwWindowShouldClose(window)){
+				running = false;
+			}
+		}
+	}
+
+}
